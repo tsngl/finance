6382920db05d3e85444cf11bda46cf3fc82b9a5e
Tsengee <tsngl9999@gmail.com>
2020-06-16 18:21:04 +0800

    Event listeneruudiig hiilee

---
+++ app.js
@@ -0,0 +1,26 @@
+//user interface Controller Modul  / Дэлгэцийн контроллер
+var uiController = (function() {})();
+
+//Finance Controller Modul / Санхүүгийн контроллер
+var financeController = (function() {})();
+
+// Connection Controller Modul / Програмын холбогч контроллер
+var appController = (function(uiCntrllr, fnCntrllr) {
+  var cntrlAddItem = function() {
+    // Оруулсан өгөгдлийг дэлгэцнээс олж авна
+    console.log("Оруулсан өгөгдлийг олж  авлаа");
+    // Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална
+    // Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+    // Төсвийг тооцоолно
+    // Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана
+  };
+  document.querySelector(".add__btn").addEventListener("click", function() {
+    cntrlAddItem();
+  });
+  document.addEventListener("keypress", function(event) {
+    console.log(event);
+    if (event.keyCode === 13 || event.which === 13) {
+      cntrlAddItem();
+    }
+  });
+})(uiController, financeController);
